395e096d2e45c9c4bf40c40832d28e381950671e
boa10 <96635920+boa10@users.noreply.github.com>
2023-03-10 16:52:17 +0100

    Add files via upload

---
+++ import java.util.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Rectangle {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getArea() {
+        return length * width;
+    }
+
+    public double getPerimeter() {
+        return 2 * (length + width);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the length of the rectangle: ");
+        double length = scanner.nextDouble();
+
+        System.out.print("Enter the width of the rectangle: ");
+        double width = scanner.nextDouble();
+
+        Rectangle rectangle = new Rectangle(length, width);
+
+        System.out.println("The area of the rectangle is: " + rectangle.getArea());
+        System.out.println("The perimeter of the rectangle is: " + rectangle.getPerimeter());
+    }
+}
